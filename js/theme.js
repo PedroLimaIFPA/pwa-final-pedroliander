c99f21bf5be0dcef843a42fb81832245621a245c
Pedro Dimas <pedrolimaifpa@gmail.com>
2024-07-08 00:00:57 -0300

    first commit

---
+++ js/theme.js
@@ -0,0 +1,35 @@
+const themeToggle = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+const logo = document.getElementById('logo');
+
+// Função para atualizar a logo com base no tema
+function updateLogo(theme) {
+  if (theme === 'dark') {
+    logo.src = 'src/Logo_tema_escuro144px.png';
+  } else {
+    logo.src = 'src/Logo_tema_claro144px.png';
+  }
+}
+
+// Inicializa o tema e a logo
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  updateLogo(currentTheme);
+
+  if (currentTheme === 'dark') {
+    themeToggle.checked = true;
+  }
+}
+
+// Adiciona o listener para alternar entre temas
+themeToggle.addEventListener('change', () => {
+  if (themeToggle.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    updateLogo('dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    updateLogo('light');
+  }
+});
