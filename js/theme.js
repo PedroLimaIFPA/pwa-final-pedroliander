fecc20222aefda6b5723960ce4225e40cc1bdd79
PedrolimaIFPA <pedrolimaifpa@gmail.com>
2024-07-10 13:15:03 -0300

    implementações-pwa

--- js/theme.js
+++ js/theme.js
@@ -38,3 +38,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/js/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }, err => {
+        console.log('Service Worker registration failed:', err);
+      });
+  });
+}
