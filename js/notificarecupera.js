e3716cc796b2cf451ed05a49f31796c2ebb7b4dd
Pedro Dimas <pedrolimaifpa@gmail.com>
2024-07-18 17:23:04 -0300

    teste notificação

--- js/notificarecupera.js
+++ js/notificarecupera.js
@@ -1,28 +1,23 @@
 // notificarecupera.js
 
 (async () => {
-    const button = document.getElementById('recover-button');
-  
-    // Solicita permissão ao carregar a página
-    if (Notification.permission !== 'granted') {
-      await Notification.requestPermission();
+  const button = document.getElementById('recover-button');
+
+  // Solicita permissão ao carregar a página
+  if (Notification.permission !== 'granted') {
+    await Notification.requestPermission();
+  }
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (Notification.permission === 'granted') {
+      new Notification('Link de recuperação enviado com sucesso', {
+        body: 'Recuperação de senha',
+        icon: '/src/Logo_tema_claro144px.png' // Certifique-se de que o caminho está correto
+      });
+    } else {
+      console.log('Permissão para notificações não concedida.');
     }
-  
-    button.addEventListener('click', (event) => {
-      event.preventDefault();
-  
-      if (Notification.permission === 'granted') {
-        const notification = new Notification('Link de recuperação enviado com sucesso', {
-          body: 'Recuperação de senha',
-          icon: '/src/Logo_tema_claro144px.png' // Certifique-se de que o caminho está correto
-        });
-  
-        console.log(notification);
-  
-        setTimeout(() => notification.close(), 4000);
-      } else {
-        console.log('Permissão para notificações não concedida.');
-      }
-    });
-  })();
-  
+  });
+})();
